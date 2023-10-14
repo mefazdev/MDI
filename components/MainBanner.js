12ca23c8df44413c28c0a2cbab6b6c22d96aa564
mefazdev <fazdev.com@gmail.com>
2023-10-14 15:12:24 +0530

    adbn

--- components/MainBanner.js
+++ components/MainBanner.js
@@ -5,7 +5,7 @@ import slide7 from "../assets/images/slide/slide-7.jpg";
  
 import slide3 from "../assets/images/slide/slide-3.jpg";
 import slide4 from "../assets/images/slide/slide-4.jpg";
-import slide5 from "../assets/images/slide/slide-5.jpg";
+import slide5 from "../assets/images/slide/slide-5.JPG";
  
 
 export default function MainBanner() {
