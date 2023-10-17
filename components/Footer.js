5b9650a4097f203f7d54d41ebc50acc976f20ceb
mefazdev <fazdev.com@gmail.com>
2023-10-17 09:41:52 +0530

    poweredby

--- components/Footer.js
+++ components/Footer.js
@@ -120,102 +120,23 @@ export default function Footer() {
           
         </div>
 
-        {/* <div className="footer__item">
-          <Link href="/">
-            <p>Home</p>
-          </Link>
-          <Link href="/About">
-            <p>About</p>
-          </Link>
-          <Link href="/Donate">
-            <p>Donate</p>
-          </Link>
-          <Link href="/Contact">
-            <p>Contact us</p>
-          </Link>
-        </div> */}
+         
 
-        {/* <div className="footer__item">
-          <Link href="/Photos">
-            <p>Photo Gallery</p>
-          </Link>
-          <Link href="/Videos">
-            <p>Video Gallery</p>
-          </Link>
-          <Link href="/Orphanage">
-            <p>Womens Orphanage</p>
-          </Link>
-          <Link href="/School">
-            <p>English School</p>
-          </Link>
-        </div> */}
-        {/* <div className="footer__item">
-          <Link href="/Hadiya">
-            <p>Hadiya Academy</p>
-          </Link>
-          <Link href="/Hadiya">
-            <p>Womens Arts</p>
-          </Link>
-          <Link href="/Zahrath">
-            <p>Zahra Preschool</p>
-          </Link>
-          <Link href="/Homecare">
-            <p>Home Care</p>
-          </Link>
-        </div> */}
-        {/* <div id="footer__adress" className="footer__item">
-          <div className="flex footer__name">
-            <ApartmentIcon id="footer__icon" />
-            <div>
-              <p>MDI KARULAI</p>
-
-              <h3>Krulai, Nilambur</h3>
-              <h3> Malappuram Kerala</h3>
-              <h3>India</h3>
-            </div>
-          </div>
-          <div className="flex footer__phone">
-            <CallIcon id="footer__icon" />
-            <p>+91 9656570068</p>
-          </div>
-          <div className="flex footer__phone">
-            <EmailIcon id="footer__icon" />
-            <p>info@mdikarulai.in </p>
-          </div>
-          <div className="footer__input w-fit grid grid-cols-4  gap-3">
-            <a href="https://www.facebook.com/mdischoolkarulai?mibextid=LQQJ4d">
-              <div className="h-8 w-8 bg-white text-cyan-900   rounded-full flex justify-center items-center">
-                <FacebookIcon />
-              </div>
-            </a>
-
-            <a
-              href='https://www.instagram.com/madaru_dawathil_islamiyya_mdi/?igshid=NzZlODBkYWE4Ng%3D%3D'
-            >
-              <div className="h-8 w-8 bg-white text-cyan-900   rounded-full flex justify-center items-center">
-                <InstagramIcon />
-              </div>
-            </a>
-           
-            <a href="https://www.youtube.com/@mdikarulai714">
-              <div className="h-8 w-8 bg-white text-cyan-900   rounded-full flex justify-center items-center">
-                <YouTubeIcon />
-              </div>
-            </a>
-          </div>
-        </div> */}
+        
+        
+         
       </div>
 
       <div className="border-t border-gray-600 p-2   mt-10 text-gray-200">
-<div className=" md:w-10/12 m-auto grid lg:grid-cols-2">
-<p className="text-sm"> © MDI Karulai 2023. <spa>All Rights reserved</spa></p> 
-{/* <p className="text-sm">Poweres by <a href="whatsapp://send?phone=+918606256402" data-action="share/whatsapp/share"   >
+<div className=" md:w-10/12 m-auto grid lg:grid-cols-2 ">
+<p className="text-sm m-auto lg:m-0"> © MDI Karulai 2023. <spa>All Rights reserved</spa></p> 
+<p className="text-sm m-auto lg:m-0">Powered by <a href="whatsapp://send?phone=+918606256402" data-action="share/whatsapp/share"   >
                  
                  <span style={{textDecoration:'underline'}}>Booreact</span>
                    
                  
                
-    </a></p> */}
+    </a></p>
 </div>
         
          {/* <h4>All rights reserved. Developed by <a href="whatsapp://send?phone=+918606256402" data-action="share/whatsapp/share"   >
